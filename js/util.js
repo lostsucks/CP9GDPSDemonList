27d0da35d13a715370c7120a383551107842e54f
Elisha <lostmatterlmao@gmail.com>
2025-02-18 01:55:25 -0600

    fractiondigits

--- js/util.js
+++ js/util.js
@@ -8,5 +8,5 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
+    return num.toLocaleString(undefined, { minimumFractionDigits: 1 });
 }
